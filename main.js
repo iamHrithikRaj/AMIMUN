8fe8dfa0525aea539edc07ed9a25fd304b7df4c3
Hrithik Raj <hrithikraj@Hrithiks-MacBook-Air.local>
2021-10-20 22:50:12 +0530

    added contact us and collaborators

--- main.js
+++ main.js
@@ -1,13 +1,52 @@
-gsap.to("#videoplayer", {
+gsap.registerPlugin(ScrollTrigger);
+
+let t = gsap.timeline({
   scrollTrigger: {
     trigger: "#main",
     start: "top top",
     scrub: 2,
     pin: "#main",
   },
-  width: "100%",
-  ease: "Expo.easeInOut",
 });
+
+t.to("#videoplayer", { width: "100%", ease: "Expo.easeInOut" });
+
+// gsap.to("#videoplayer", {
+//   scrollTrigger: {
+//     trigger: "#main",
+//     start: "top top",
+//     scrub: 2,
+//     pin: "#main",
+//   },
+//   width: "100%",
+//   ease: "Expo.easeInOut",
+// });
+
+
+let t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#main",
+    start: "top top",
+    scrub: 2,
+    pin: "#main",
+  },
+});
+
+t1.to(".videoplayer-center-text", { opacity: "1", ease: "Expo.easeInOut", visibility: "visible" }, 0.3);
+
+// gsap.to(
+//   ".videoplayer-center-text",
+//   {
+//     scrollTrigger: {
+//       trigger: "#main",
+//       start: "top top",
+//       scrub: 1,
+//     },
+//     opacity: 1,
+//     ease: "Expo.easeInOut",
+//   },
+//   1000
+// );
 
 gsap.to("#one", {
   scrollTrigger: {
@@ -53,3 +92,12 @@ gsap.to("#holder2", {
   opacity: 0,
 });
 
+anime.timeline().add({
+  targets: ".header",
+  opacity: [0, 1],
+  translateY: [80, 0],
+  translateZ: 0,
+  easing: "easeOutExpo",
+  duration: 2000,
+  delay: (el, i) => 300 + 40 * i,
+});
