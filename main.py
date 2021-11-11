51e409431e46cfab6fd0a543b6b13b2b78520698
iamHrithikRaj <iamHrithikRaj26@gmail.com>
2021-11-11 16:22:57 +0530

    added gallery

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+f_obj = open("./gallery-data.txt", "w")
+
+for i in os.listdir(os.getcwd() + "/gallery-imgs"):
+    data =  '''<a
+            href=" + ./gallery-imgs/'''+ i  + '''"
+            data-lightbox="models"
+            data-title="Caption1"
+          >
+            <img
+              src="./gallery-imgs/'''+i+'''"
+            />
+          </a>'''
+
+    f_obj.write(data)
+    f_obj.write("\n")
